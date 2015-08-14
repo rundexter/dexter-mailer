fb89c21e1272dfc62931049932c8c66073165068
Justin Fargione <justin@lexicalbits.com>
2015-08-14 11:41:07 -0400

    Wired up to Dexter endpoint

--- index.js
+++ index.js
@@ -14,7 +14,13 @@ module.exports = {
      */
     , run: function(step, dexter) {
         var to, from, subject, bodyText, bodyHtml, cc, bcc
+            , api_key = dexter.user('profile').api_key
+            , url     = dexter.url('home')+'api/smtp/?api_key='+api_key
+            , restler = require('restler')
             ;
+        if(!api_key) {
+            return this.fail('No API key provided');
+        }
         if((to = step.input('to', null)) === null) {
             return this.fail('"to" field is required');
         }
@@ -31,15 +37,29 @@ module.exports = {
            return this.fail('Either "text" (for a text-based email), "html" (for an html-based email), or both are required');
         } 
         cc = step.input('cc', null);
-        cc = step.input('bcc', null);
-        this.complete({
-            to: to
-            , from: from
-            , subject: subject
-            , text: bodyText
-            , html: bodyHtml
-            , cc: cc
-            , bcc: bcc
-        });
+        bcc = step.input('bcc', null);
+
+        restler.post(url, {
+            data: {
+                to: to
+                , from: from
+                , subject: subject
+                , text: bodyText
+                , html: bodyHtml
+                , cc: cc
+                , bcc: bcc
+            }
+            , headers: {
+                'X-Authorization': api_key
+            }
+        }).on('complete', function(result, response) {
+            if(result instanceof Error) return this.fail(result);
+            return response.statusCode == 200 
+                ? this.complete({ noop: true })
+                : this.fail({
+                    message: 'Unexpected Response From Server ' + response.statusCode,
+                    data: result
+                });
+        }.bind(this));
     }
 };
